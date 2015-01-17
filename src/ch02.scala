30423b69c57b3aafc72f574d8137d4080a4842b1
tianbingchao <wjt_bc@163.com>
2015-01-17 17:27:55 +0800

    started

--- src/ch02.scala
+++ src/ch02.scala
@@ -12,3 +12,15 @@ args.foreach(arg => println(arg))
 
 
 args.foreach(println)
+
+var twoThree = List(2,3)
+var oneTwoThree = 1 :: twoThree
+println(oneTwoThree)
+
+var pair = (459, "看看 咖啡男")
+println(pair._1)
+println(pair._2)
+
+var jetSet = Set("Boeing", "AirBus")
+jetSet += "Linear"
+println(jetSet.contains("Cessna"))
